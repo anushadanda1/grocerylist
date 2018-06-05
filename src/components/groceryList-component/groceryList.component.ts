e5d6d3da63e81cd3575b354a084db1d434106c86
narayan reddy <narayan.reddy@globallogic.com>
2018-06-05 11:07:57 +0530

    sample code

---
+++ src/components/groceryList-component/groceryList.component.ts
@@ -0,0 +1,97 @@
+import { Component } from '@angular/core';
+import {} from '../../assets/images/'
+
+@Component({
+  selector: 'app-grocery-list',
+  templateUrl: './groceryList.component.html',
+  styleUrls: ['./groceryList.component.scss']
+})
+export class GroceryListComponent {
+  title = 'app works!';
+
+  grocerys = [
+    {
+      'category': 'fruit',
+      'item': 'apples',
+      'type': 'Honey Crisp',
+      'brand': 'Little cuties',
+      'qty': 10
+    },
+    {
+      'category': 'beverage',
+      'item': 'milk',
+      'type': '2%',
+      'brand': 'generic',
+      'qty': '1 gallon'
+    },
+    {
+      'category': 'pasta',
+      'item': 'Pasta',
+      'type': 'Penne',
+      'brand': 'Barilla',
+      'qty': 3
+    },
+    {
+      'category': 'dessert',
+      'item': 'Gelatin dessert',
+      'type': 'Green',
+      'brand': 'Jello',
+      'qty': 3
+    },
+    {
+      'category': 'dairy',
+      'item': 'Yogurt',
+      'type': 'Assorted flavors',
+      'brand': 'Chobani',
+      'qty': 12
+    },
+    {
+      'category': 'pasta',
+      'item': 'Pasta',
+      'type': 'Linguini',
+      'brand': 'Barilla',
+      'qty': 3
+    },
+    {
+      'category': 'beverage',
+      'item': 'Apple juice',
+      'type': 'regular',
+      'brand': 'Happy Farms',
+      'qty': 2
+    },
+    {
+      'category': 'beverage',
+      'item': 'Vodka',
+      'type': 'Tangerine',
+      'brand': 'Grey Goose',
+      'qty': 1
+    }
+  ];
+
+  private getIconUrl(type: string): string {
+    let url: string;
+    if (type.toLowerCase()) {
+      switch (type.toLowerCase()) {
+        case 'fruit':
+          url = 'assets/images/fruit.svg';
+          break;
+        case 'dairy':
+          url = 'assets/images/dairy.svg';
+          break;
+        case 'beverage':
+          url = 'assets/images/beverage.svg';
+          break;
+        case 'pasta':
+          url = 'assets/images/pasta.svg';
+          break;
+        case 'dessert':
+          url = 'assets/images/dessert.svg';
+          break;
+        default:
+          url = 'assets/images/defaultImg.svg';
+          break;
+      }
+    }
+    return url;
+}
+}
